8155f95a1ac7b5d6eaaf11d488deb6ba459ec1fc
SANKEERTH569 <126935273+SANKEERTH569@users.noreply.github.com>
2023-03-04 10:14:17 +0530

    /sample output
    Enter number a : 2
    Enter number b : 3
    Enter number c : 6
    c is bigger

---
+++ greatest among three number.py
@@ -0,0 +1,9 @@
+a=int(input("Enter number a : "))
+b=int(input("Enter number b : "))
+c=int(input("Enter number c : "))
+if(a>b and a>c):
+    print("a is bigger")
+elif(b>a and b>c):
+    print("b is bigger")
+else:
+    print("c is bigger")
